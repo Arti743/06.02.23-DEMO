8e4b5d4e76a28236b19a28d2d8f07f64af1a6300
Arti743 <grebenshchekov.2016@Inbox.ru>
2023-02-07 20:50:27 +0700

    First commit
    First committe

---
+++ Dostavka.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Demo
+{
+    public partial class Dostavka : Form
+    {
+        DataSet data;
+        SqlDataAdapter adap;
+        //Задаем адрес подключения к нашему Sql Server.
+        //Data Source - Имя(ip) сервера.
+        //Initial Catalog - Имя базы данных к которой мы подключаемся.
+        //Integrated Security - проверка подлинности. В нашем случае проверка подлинности Windows.
+        string connectionString = @"Data Source=192.168.0.3, 3306; Initial Catalog=Demo; Integrated Security=True";
+        //Вывод таблицы через команду Sql.
+        string sql = "SELECT * FROM delivery";
+        public static bool Sett;
+
+        public Dostavka()
+        {
+            InitializeComponent();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToAddRows = false;
+
+            //Осуществляем подключение к серверу и вывод данных в dataGridView.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    adap = new SqlDataAdapter(sql, connection);
+
+                    data = new DataSet();
+                    adap.Fill(data);
+                    dataGridView1.DataSource = data.Tables[0];
+                    dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                    dataGridView1.AllowUserToAddRows = false;
+                }
+                //При отсутсвии подключения к базе данных(среверу), будет выводиться сообщение об этом.
+                catch
+                {
+                    MessageBox.Show("Отсутсвует подключение к серверу. Повторите попытку позже", "Ошибка подключения");
+                    Application.Restart();
+                }
+            }
+        }
+        //При нажатии на строку в dataGridView будет выходить сообщение с данными выбранной строки.
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                sb.Append(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[i].Value + " | ");
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
+        //Обработчик события закрытия формы через "крестик".
+        private void Dostavka_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //При нажатии кнопки "Выйти из аккаунта" будет осуществляться переход на окно авторизации.
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Form1 Win = new Form1();
+            Win.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Обработчик события нажатия кнопки "Войти".
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Задаем адрес подключения к нашему Sql Server.
+            //Data Source - Имя(ip) сервера.
+            //Initial Catalog - Имя базы данных к которой мы подключаемся.
+            //Integrated Security - проверка подлинности. В нашем случае проверка подлинности Windows.
+            string connectionString = @"Data Source=192.168.0.3, 3306; Initial Catalog=Demo; Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+            bool succes = false;
+            try
+            {
+                //Указываем в каких поля содержится логин и пароль.
+                const string comand = "SELECT * FROM users WHERE user_log=@user_log AND user_pwd=@user_pwd";
+                SqlCommand check = new SqlCommand(comand, conn);
+                //Даем ссылку на наши textbox'ы, которые будут вводится данные аккаунта.
+                check.Parameters.AddWithValue("@user_log", textBox1.Text);
+                check.Parameters.AddWithValue("@user_pwd", textBox2.Text);
+                conn.Open();
+
+                using (var dataReader = check.ExecuteReader())
+                {
+                    succes = dataReader.Read();
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+            //Если введенные данные верны, осуществляем переход на форму с таблицей.
+            if (succes)
+            {
+                Dostavka Win = new Dostavka();
+                Win.Show();
+                this.Hide();
+            }
+            //Если данные НЕ верны, выводим сообщение об этом.
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!");
+            }
+        }
+        //Обработчик события закрытия формы через "крестик".
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
